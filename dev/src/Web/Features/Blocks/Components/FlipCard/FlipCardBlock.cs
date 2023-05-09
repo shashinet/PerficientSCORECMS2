a62cbdd0019ab71dab8c55837d1f57185013722c
shashiopti <shashikant.bhoyar@perficient.com>
2023-05-09 10:50:19 +0530

    Flipcard Implementation

--- dev/src/Web/Features/Blocks/Components/FlipCard/FlipCardBlock.cs
+++ dev/src/Web/Features/Blocks/Components/FlipCard/FlipCardBlock.cs
@@ -9,6 +9,7 @@ using Perficient.Infrastructure.EditorDescriptors.Colors;
 using Perficient.Infrastructure.Interfaces.BlockTypes;
 using Perficient.Infrastructure.Models.Base;
 using Perficient.Web.Features.Blocks.Components.RichText;
+using Perficient.Web.Features.Blocks.Fields.Icon;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Drawing.Imaging;
@@ -27,14 +28,14 @@ namespace Perficient.Web.Features.Blocks.Components.FlipCard
         [CultureSpecific]
         [Display(
            Order = 10,
-           GroupName = SystemTabNames.Content,
+           GroupName = TabNames.FrontOfCard,
            Name = "Title"
        )]
         public virtual string Title { get; set; }
 
         [Display(
            Order = 20,
-           GroupName = SystemTabNames.Content,
+           GroupName = TabNames.FrontOfCard,
            Name = "Description"
        )]
         [CultureSpecific]
@@ -42,14 +43,28 @@ namespace Perficient.Web.Features.Blocks.Components.FlipCard
         public virtual ContentArea Description { get; set; }
 
         [CultureSpecific]
-        [Display(GroupName = SystemTabNames.Content, Name = "CTA Button", Order = 30)]
-        [AllowedTypes(typeof(ICallToActionBlock))]
-        public virtual ContentArea CallToActionButton { get; set; }
+        [Display(GroupName = TabNames.FrontOfCard, Name = "Button Label", Order = 30)]
+      
+        public virtual string CallToActionLabel { get; set; }
 
         [CultureSpecific]
-        [Display(GroupName = SystemTabNames.Content, Name = "Solid Color", Order = 30)]
+        [Display(GroupName = TabNames.BackOfCard, Name = "Solid Color", Order = 10)]
         [EditorDescriptor(EditorDescriptorType = typeof(ColorPickerEditorDescriptor))]
         [UIHint("ColorPickerEditor")]
-        public virtual ContentArea CallToActionButton1 { get; set; }
+        public virtual ContentArea SolidColor { get; set; }       
+
+        [Display(
+          Order = 20,
+          GroupName = TabNames.BackOfCard,
+          Name = "Text"
+      )]
+        [CultureSpecific]
+        [AllowedTypes(new[] { typeof(RichTextBlock) })]
+        public virtual ContentArea Text { get; set; }
+
+        [CultureSpecific]
+        [Display(GroupName = TabNames.BackOfCard, Name = "CTA Button", Order = 30)]
+        [AllowedTypes(typeof(ICallToActionBlock))]
+        public virtual ContentArea CallToActionButton { get; set; }
     }
 }
